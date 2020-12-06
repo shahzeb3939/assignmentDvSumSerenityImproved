79cfad087a1a26bd535617512e452b407a787fd5
shahzeb <shahzeb3939@gmail.com>
2020-12-06 11:26:25 +0500

    Closing commit on EditViewSettingsPage branch.

--- src/main/java/serenity/dvsum/steps/EditView.java
+++ src/main/java/serenity/dvsum/steps/EditView.java
@@ -11,11 +11,11 @@ public class EditView {
         return Task.where("{0} edits the view",
                 SelectViewFromDropdown.named(viewData.get("viewName")),
                 SelectViewSetting.editView(),
-//                SetNewViewName.named(viewData.get("newViewName")),
-//                SetDatabaseType.named(viewData.get("databaseType")),
-//                SetDatabaseFilter.named(viewData.get("databaseFilter")),
-//                SetDatabaseName.named(viewData.get("databaseName")),
+                SetNewViewName.named(viewData.get("newViewName")),
                 SelectColumns.named(viewData.get("columnsToBeSelected")),
+                SetDatabaseType.named(viewData.get("databaseType")),
+                SetDatabaseFilter.named(viewData.get("databaseFilter")),
+                SetDatabaseName.named(viewData.get("databaseName")),
                 SaveChanges.madeToView()
         );
     }
